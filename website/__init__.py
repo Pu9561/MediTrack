497943ac5adb355753db967b9aedeaaa78f9490d
TheoHwang <126973740+TheoHwang@users.noreply.github.com>
2023-03-04 18:21:03 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
+
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']=='MediTrack'
+    
     
     from .views import views
     from .auth import auth
