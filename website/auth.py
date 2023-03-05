0dbabf8905a7a075a33b9e36cac859d371f3f8ea
TheoHwang <126973740+TheoHwang@users.noreply.github.com>
2023-03-05 03:10:15 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -16,5 +16,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
-  
+  data=request.form
+  print(data)
+  if request.method=='POST':
+    email=request.form.get('email')
+    password1=request.form.get('password1')
+    password2=request.form.get('password2')
+    firstName=request.form.get('firstName')
+    lastName=request.form.get('lastName')
+    birthMonth=request.form.get('m')
+    birthDay=request.form.get('day')
+    birthYear=request.form.get('year')
+    gender=request.form.get('gender')
+    height=request.form.get('height')
+    weight=request.form.get('weight')
+    
+
+
   return render_template('signUp.html')
