526d0157c6eaf59d97673e893a76660717292a16
TheoHwang <126973740+TheoHwang@users.noreply.github.com>
2023-03-05 08:23:46 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -13,6 +13,9 @@ def login():
 def logout():
   return "<p>logout</p>"
 
+@auth.route('/index')
+def index():
+   return render_template('index.html')
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -24,50 +27,18 @@ def sign_up():
     password2=request.form.get('password2')
     firstName=request.form.get('firstName')
     lastName=request.form.get('lastName')
-    m=request.form.get('m')
-    day=request.form.get('day')
-    year=request.form.get('year')
+    birthDay=request.form.get('date')
     gender=request.form.get('gender')
     height=request.form.get('height')
     weight=request.form.get('weight')
 
 
-    months = ["01", "02", "03", "04", "05", "06" ,"07" ,"08" ,"09" ,"10" ,"11" ,"12"]
-    days = []
-    for i in range(1,32):
-      stri = str(i)
-      if len(stri)<2:
-        stri = "0"+stri
-      
-      days.append(stri)
-    
-      
-
-    if len(email) < 4:
-      flash('Email must be greater than 4 characters.', category = 'error')
-    elif password1 != password2:
-      flash('Passwords do not match.', category = 'error')
-    elif len(password1)< 7:
-      flash('Password must be greater than 7 characters.', category = 'error')
-    elif len(firstName) < 2:
-      flash('Firstname must be greater than 2 characters.', category = 'error')
-    elif len(lastName) < 2:
-      flash('Lastname must be greater than 2 characters.', category = 'error')
-    elif m not in months:
-      flash('Invalid month, if month is single digit it must have a 0 before it.', category = 'error')
-    elif day not in days:
-      flash('Invalid day, if day is single digit it must have a 0 before it.', category = 'error')
-    elif len(year)!=4:
-      flash('Year must be 4 digits long.', category = 'error')
-    elif len(gender)>10:
-      flash('Gender must be less than 10 characters.', category = 'error')
-    elif height<0:
-      flash('Invalid height, height must be greater than 0.', category = 'error')
-    elif weight<0:
-      flash('Invalid height, height must be greater than 0.', category = 'error')
-    else:
-      flash('Account Created!', category = 'success')
-    
-
 
   return render_template('signUp.html')
+@auth.route('/enter-info', methods=['GET', 'POST'])
+def enter_info():
+ if request.method=="POST":
+  d=request.form.get('date')
+  print(d)
+  print(type(d))
+ return render_template ("enterInfo.html")
